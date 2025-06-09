349e4665fad5fb72601bfe40ed9c171c1008fbf4
Isaque Barbosa <isaquebatista400@gmail.com>
2025-06-09 19:16:32 -0300

    :heavy_plus_sign: Add react-error-boundary for error tratament

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from 'react-error-boundary'
 import App from './App.tsx'
 
+function fallbackRender({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ErrorBoundary
+  fallbackRender={fallbackRender}
+>
+  <App />
+</ErrorBoundary>
 )
